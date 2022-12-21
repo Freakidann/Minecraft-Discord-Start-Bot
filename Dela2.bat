623a4173b863640b7aaf387c423532ca2edf4efa
Freakidann <99401816+Freakidann@users.noreply.github.com>
2022-12-21 12:07:47 +0100

    Add files via upload

---
+++ Dela2.bat
@@ -0,0 +1,4 @@
+@echo off
+cd DelaHost1\
+java -Xms3000M -Xmx3000M -jar spigot-1.16.5.jar --nogui
+pause
---
+++ Dela3.bat
@@ -0,0 +1,4 @@
+@echo off
+cd DelaHost3\
+java -Xms1000M -Xmx1000M -jar spigot-1.16.5.jar --nogui
+pause
